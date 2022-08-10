992aa81818013b395adeef3475f41cafacfc91dc
Cosmin Poieana <cosmin@robocorp.com>
2022-08-10 16:40:06 +0200

    No need to monkeypatch updated exchangelib (OAuth2)

--- ExtendedExchange.py
+++ ExtendedExchange.py
@@ -1,25 +1,8 @@
-import functools
-
 from RPA.Email.Exchange import Exchange
 from RPA.Robocorp.Vault import Vault
-from exchangelib.services import common as exchangelib_common
-from exchangelib.util import post_ratelimited
 
 
 vault = Vault()
-
-
-def extended_post_ratelimited(protocol, session, *args, **kwargs):
-    session.post = functools.partial(
-        session.post,
-        client_id=protocol.credentials.client_id,
-        client_secret=protocol.credentials.client_secret,
-    )
-    return post_ratelimited(protocol, session, *args, **kwargs)
-
-# Monkey-patching infinite loop fix on token refresh, due to OAuth2 library bug when
-#  passing the app credentials to the "/token" endpoint.
-exchangelib_common.post_ratelimited = extended_post_ratelimited
 
 
 class ExtendedExchange(Exchange):
